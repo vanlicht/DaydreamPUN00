a7bdfb94ab65e83bdb8d7f10e6771f4252e22dcb
Thomas Huang <vanlicht@gmail.com>
2017-05-08 09:22:34 -0700

    first commit

---
+++ Assets/Thomas/Scripts/PhotonCommsManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon;
+
+public class PhotonCommsManager : Photon.PunBehaviour
+{
+    #region Public Variables
+    public PhotonLogLevel logLevel = PhotonLogLevel.Informational;
+    #endregion
+
+    #region Private Behaviour
+    private GameObject currentPlayer;
+    #endregion
+
+    #region MonoBehaviours
+    private void Awake()
+    {
+        PhotonNetwork.autoJoinLobby = false;
+    }
+    void Start ()
+    {
+        PhotonNetwork.logLevel = logLevel;
+        PhotonNetwork.ConnectUsingSettings("0.1");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+    #endregion
+
+    #region Public Methods
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Trying to enter random room.");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnPhotonRandomJoinFailed(object[] codeAndMsg)
+    {
+        Debug.Log("Can't join random room!");
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        currentPlayer = PhotonNetwork.Instantiate("DaydreamPlayer", new Vector3(0f, 1.6f, 0f), Quaternion.identity, 0);
+        currentPlayer.GetComponent<PlayerController>().isControllable = true;
+    }
+    #endregion
+
+    #region GUI
+    private void OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+    #endregion
+}
---
+++ Assets/Thomas/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject gvrControllerPointer;
+    public GameObject head;
+    public GameObject otherPlayersController;
+    public GameObject playerCamera;
+
+    public bool isControllable;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (isControllable)
+        {
+            TeleportEvent teleportEvent = GameObject.Find("TeleportController").GetComponent<ThomasTeleportController>().teleportEvent;
+            teleportEvent.AddListener(HandleTeleportEvent);
+            playerCamera.SetActive(true);
+            gvrControllerPointer.SetActive(true);
+            head.SetActive(false);
+            otherPlayersController.SetActive(false);
+        }
+        else
+        {
+            playerCamera.SetActive(false);
+            gvrControllerPointer.SetActive(false);
+        }
+	}
+
+    private void HandleTeleportEvent(Vector3 worldPos)
+    {
+        gameObject.transform.position = new Vector3(worldPos.x, gameObject.transform.position.y, worldPos.z);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
